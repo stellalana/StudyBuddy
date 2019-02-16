e0f5b1ad757a2d36af09eb1b132163b4b96c08ae
John Peters <peters.johnathan@gmail.com>
2019-02-16 10:59:11 -0500

    updated navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,11 +7,13 @@ import "./style.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg d-flex flex-row-reverse">
       <LoginForm />
+      <div className="mr-auto p-2">
+      <Link to="/">Home</Link><span style={{padding:'10px'}}>|</span>
       <Link to="/getQuestions">Add Questions</Link><span style={{padding:'10px'}}>|</span>
       <Link to="/test">Start Quiz</Link>
-      
+      </div>
 
     </nav>
   );
